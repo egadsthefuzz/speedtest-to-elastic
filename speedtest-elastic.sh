93dd9332df4299bac0ea7828ebda65e166b3ad3f
egadsthefuzz <william.g.reynolds@egadsthefuzz.com>
2017-10-27 22:53:34 +1100

    Update speedtest-elastic.sh

--- speedtest-elastic.sh
+++ speedtest-elastic.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+speedtestdirectory=/opt/speedtest-cli
+es_url=localhost
 number=$RANDOM;
 let "number %= 9";
 let "number = number + 1";
@@ -10,7 +11,7 @@ for i in {1..18}; do
   let "r %= $range";
   number="$number""$r";
 done;
-json=`/opt/speedtest-cli/speedtest-cli --secure --json`
+json=`"$speedtestdirectory"/speedtest-cli --secure --json`
 #echo $number
 #echo $json
-curl -XPUT -H 'Content-Type: application/json' http://localhost:9200/speedtest/test/"$number" -d"$json"
+curl -XPUT -H 'Content-Type: application/json' http://"$es_url":9200/speedtest/test/"$number" -d"$json"
